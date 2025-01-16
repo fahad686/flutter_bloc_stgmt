ddddea03d4b89aa5ae121d9e100cb6453363e606
Fahad Ali <fahad.ali@schuja.com>
2025-01-16 13:14:04 +0500

    equatable simple example with equatable  package

--- flutter_bloc/lib/main.dart
+++ flutter_bloc/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/%20equatable_with_package.dart';
 import 'package:flutter_bloc/equtable_testing.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ), 
-      home:  EquatableTesting(),
+      home:  EquatableWithPackage(),
     );
   }
 }
